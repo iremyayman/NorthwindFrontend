e97282a802bf26068bd34905575de89a4c39f341
İrem <iremyayman12@gmail.com>
2021-03-24 02:30:48 +0300

    24.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',//süslü parantez içi bir objedir
+  styleUrls: ['./app.component.css']//köşeli parantez array demek
 })
 export class AppComponent {
-  title = 'northwind';
+  title:string = 'northwind';
+  user:string='İrem Yayman';
+ 
 }
+//datayı yönettiğimiz yer,angularda her şey class
